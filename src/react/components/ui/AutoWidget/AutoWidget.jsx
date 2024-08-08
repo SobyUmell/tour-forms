d3a86e0a5a15cabd574bb571fd04e2f89fbe58db
Sobar <sdruza4@gmail.com>
2024-08-08 17:46:41 +0300

    Added Submit widget to AutoWidget

--- src/react/components/ui/AutoWidget/AutoWidget.jsx
+++ src/react/components/ui/AutoWidget/AutoWidget.jsx
@@ -3,25 +3,23 @@ import ImageWidget from "../ImageWidget/ImageWidget";
 import InputWidget from "../../inputs/InputWidget";
 import TitleWidget from "../TitleWidget";
 import VideoWidget from "../VideoWidget";
+import SubmitWidget from "../../inputs/Submitwidget/SubmitWidget";
 
 const AutoWidget = ({widget}) => {
   const type = widget.name.split('_')[0];
   switch(type) {
     case 'CONT':
       return <ContWidget widget={widget} />
-      break;
     case 'IMAGE':
       return <ImageWidget widget={widget} />
-      break;
     case 'INPUT':
       return <InputWidget widget={widget} />
-      break;
+    case 'SUBMIT':
+      return <SubmitWidget widget={widget} />
     case 'TITLE':
       return <TitleWidget widget={widget} />
-      break;
     case 'VIDEO':
       return <VideoWidget widget={widget} />
-      break;
     default:
       return <div>The widget've not found</div>
   }
